b550ba5c0e5446a4318a474498585eda8cea75a0
Aymeric <lemimic213@gmail.com>
2017-11-28 14:40:06 -0700

    Updated Gitignore and added day2

---
+++ programming/day2/src/IntGrid2D.java
@@ -0,0 +1,35 @@
+public class IntGrid2D implements IIntGrid2D{
+    private int upleftx;
+    private int uplefty;
+    private int downrightx;
+    private int downrighty;
+    private char fill;
+    char[][] grid = new char[3][3];
+
+
+    public IntGrid2D(int ulx, int uly, int drx, int dry, char fill ){
+        upleftx = ulx;
+        uplefty = uly;
+        downrightx = drx;
+        downrighty = dry;
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0 ; j < grid.length; j++){
+                grid[i][j] = fill;
+            }
+        }
+
+    }
+    public void setPoint(IIntPoint2D p, char v){
+        grid[p.getX()+ 1][-(p.getY())+1] = v;
+
+    }
+    public char getPoint(IIntPoint2D p){
+        return grid[p.getX()+ 1][-(p.getY())+1];
+    }
+    public IIntPoint2D getUpperLeftCorner(){
+        return new IntPoint2D(upleftx, uplefty);
+    }
+    public IIntPoint2D getLowerRightCorner(){
+        return new IntPoint2D(downrightx, downrighty);
+    }
+}
---
+++ programming/day2/src/IntPoint2D.java
@@ -0,0 +1,30 @@
+public class IntPoint2D implements IIntPoint2D {
+    private int x;
+    private int y;
+    public IntPoint2D(int _x, int _y){
+        x = _x;
+        y = _y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public int manhattanDistance(IIntPoint2D o){
+        return Math.abs(x- o.getX()) + Math.abs(y- o.getY());
+    }
+    public double distance(IIntPoint2D o){
+        double total = (Math.pow(x-o.getX(), 2)) + (Math.pow(y - o.getY(), 2));
+        return Math.sqrt(total);
+    }
+    public String toString(){
+        return "(" + x + "," + y + ")";
+    }
+    public boolean equals(IntPoint2D o){
+        return x == o.getX() && y == o.getY();
+    }
+    public int hashcode(){
+        return (x<<16)+y;
+    }
+}
