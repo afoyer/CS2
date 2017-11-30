9dd51fe2ca6e8f37c4c7bc1136d28deac4bd1dec
Aymeric <lemimic213@gmail.com>
2017-11-29 18:22:39 -0700

    Updated IntGrid2D to work with more sizes

--- programming/day2/src/IntGrid2D.java
+++ programming/day2/src/IntGrid2D.java
@@ -3,7 +3,9 @@ public class IntGrid2D implements IIntGrid2D{
     private int uplefty;
     private int downrightx;
     private int downrighty;
-    private char[][] grid = new char[3][3];
+    int width;
+    int height;
+    private char[][] grid;
 
 
     public IntGrid2D(int ulx, int uly, int drx, int dry, char fill ){
@@ -11,12 +13,25 @@ public class IntGrid2D implements IIntGrid2D{
         uplefty = uly;
         downrightx = drx;
         downrighty = dry;
+        if (ulx < 0 && drx > 0){
+            width = Math.abs(ulx) + drx + 1;
+
+        }
+        if (uly > 0 && dry < 0){
+            height = Math.abs(dry) + uly + 1;
+        }
+        else{
+            width = Math.abs(ulx) +Math.abs(drx);
+            height = Math.abs(uly) + Math.abs(drx);
+        }
+        grid = new char[width][height];
+        System.out.println(width);
+        System.out.println(height);
         for (int i = 0; i < grid.length; i++){
             for (int j = 0 ; j < grid.length; j++){
                 grid[i][j] = fill;
             }
         }
-
     }
     public void setPoint(IIntPoint2D p, char v){
         grid[p.getX()+ Math.abs(upleftx)][-(p.getY())+Math.abs(uplefty)] = v;
